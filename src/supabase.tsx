cd8f89552f3825280d5c2a73708cc9f1838546af
LeviAvinje <leviandrevinje@gmail.com>
2026-05-12 08:21:54 +0200

    smol change

--- src/supabase.tsx
+++ src/supabase.tsx
@@ -1,7 +1,7 @@
 // supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-NEXT_PUBLIC_SUPABASE_URL=https://srymsxhtuiqqcyzrcshk.supabase.co/rest/v1/
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_JaVPKZee-nk3YCfT5_g9Fg_g0apfe61
+const supabaseUrl = 'https://srymsxhtuiqqcyzrcshk.supabase.co'
+const supabaseAnonKey = 'sb_publishable_JaVPKZee-nk3YCfT5_g9Fg_g0apfe61'
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
